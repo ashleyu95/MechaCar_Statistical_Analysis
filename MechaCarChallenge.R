bef348995ef9e4efef0c2148727a6c59f3c06941
yyu52 <yyu52@fordham.edu>
2021-03-28 14:07:16 +1300

    T-Tests on Suspension Coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,8 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 suspenData <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 total_summary <- suspenData %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 lot_summary <- suspenData %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+t.test(x=suspenData$PSI, mu=1500)
+t.test(x=lot_summary$Mean, subset = lot_summary$Manufacturing_Lot == "Lot1", mu=1500)
+t.test(x=lot_summary$Mean, subset = lot_summary$Manufacturing_Lot == "Lot2", mu=1500)
+t.test(x=lot_summary$Mean, subset = lot_summary$Manufacturing_Lot == "Lot3", mu=1500)
